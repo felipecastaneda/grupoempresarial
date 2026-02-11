1b2a57af0b60c6d07118a94ae8decdecac612f89
Felipe Castaneda <admin@applicationintelligence.ai>
2026-02-11 07:05:56 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5101791821-e9f7b",
+  "appId": "1:1004875851510:web:d0b2198d51008b47336ed7",
+  "apiKey": "AIzaSyDNZn_ioY1IY1VMGelau4w_dxmwWVb9D2w",
+  "authDomain": "studio-5101791821-e9f7b.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1004875851510"
+};
