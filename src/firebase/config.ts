d7de58bf1c57c5c5f1ac1cad4d75486bfc134f9e
Felipe Castaneda <admin@applicationintelligence.ai>
2026-02-11 17:33:14 +0000

    Is there anything that I need to do in the Console to allow for app auth

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "studio-5101791821-e9f7b",
+  "projectId": "company-website-ba4a8",
   "appId": "1:1004875851510:web:d0b2198d51008b47336ed7",
   "apiKey": "AIzaSyDNZn_ioY1IY1VMGelau4w_dxmwWVb9D2w",
-  "authDomain": "studio-5101791821-e9f7b.firebaseapp.com",
+  "authDomain": "company-website-ba4a8.firebaseapp.com",
   "measurementId": "",
   "messagingSenderId": "1004875851510"
 };
