2de793ef109975672a154fe549eb279bd81925d9
Felipe Castaneda <admin@applicationintelligence.ai>
2026-02-13 14:35:10 +0000

    Error only in deployed code - prod. Failed to get a response from AI. Wo

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,7 +1,9 @@
+'use server';
+
 import {genkit} from 'genkit';
-import {vertexAI} from '@genkit-ai/google-genai';
+import {googleAI} from '@genkit-ai/google-genai';
 
 export const ai = genkit({
-  plugins: [vertexAI({location: "global"})],
-  model: 'vertexai/gemini-2.5-flash',
+  plugins: [googleAI()],
+  model: 'gemini-1.0-pro',
 });
