0bcaf90439a254845301db901d1f87ece308c19b
Felipe Castaneda <admin@applicationintelligence.ai>
2026-02-13 15:54:59 +0000

    The model name you’re requesting does not exist in Vertex AI, OR your pr

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,9 +1,14 @@
-import {genkit} from 'genkit';
-import {vertexAI} from "@genkit-ai/google-genai";
+import { genkit } from 'genkit';
+import { googleAI, vertexAI } from '@genkit-ai/google-genai';
 
 export const ai = genkit({
   plugins: [
-    vertexAI({location: "global"})
+    process.env.NODE_ENV === 'production'
+      ? vertexAI({ location: 'us-central1' })
+      : googleAI({ apiKey: process.env.GOOGLE_GENAI_API_KEY }),
   ],
-  model: vertexAI.model("gemini-1.0-pro"),
+  model:
+    process.env.NODE_ENV === 'production'
+      ? vertexAI.model('google/gemini-1.5-flash')
+      : googleAI.model('gemini-1.5-pro-001'),
 });
