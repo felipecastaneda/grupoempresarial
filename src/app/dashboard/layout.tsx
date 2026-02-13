4889694849e317db7242e67cdbcb793f3e055733
Felipe Castaneda <admin@applicationintelligence.ai>
2026-02-13 04:43:53 +0000

    /src/app/dashboard/layout.tsx (3:15)
    Parsing ecmascript source code fai

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, 'use a';
+import React, { useEffect } from 'react';
 import { useRouter, usePathname } from 'next/navigation';
 import Link from 'next/link';
 import { useAuth } from '@/hooks/use-auth';
